c2e22bac2b701bbdf5e2eff32f1be88b6542379b
8BitFishy <sowerbutts.pg@hotmail.com>
2023-11-27 10:25:25 +0000

    Files for deployment

--- Command_Centre.py
+++ Command_Centre.py
@@ -1,130 +1,199 @@
+from time import sleep, ctime
+from os import listdir, system, remove
+from os.path import isfile, join
 from gpiozero import LED
-import os
-import subprocess
-import sys
-from time import sleep
+
 led = LED(17)
 
+directory = __file__.strip("Command_Centre.py").strip(":")
+protected_files = ['Command_Centre.py', 'Telegram_Manager.py', 'wollybot.py', 'telegramID.txt']
+git_repo = 'https://raw.githubusercontent.com/8BitFishy/wollybot/main/'
+
+def update():
+    system("rm wollybot/Command_Centre.py")
+    system("rm wollybot/Telegram_Manager.py")
+    system("rm wollybot/wollybot.py")
+    system(f"wget -P {directory} {git_repo}Command_Centre.py")
+    system(f"wget -P {directory} {git_repo}Telegram_Manager.py")
+    system(f"wget -P {directory} {git_repo}wollybot.py")
+    return
+
+def handle_error(E, Octavius_Receiver):
+    Octavius_Receiver.send_message("Action failed - " + E.__class__.__name__)
+    print(ctime() + " - failed with exception:")
+    print(E)
+    return
+
+def talk(Octavius_Receiver):
+    Octavius_Receiver.send_message("I am active. My current commands are: ")
+    Octavius_Receiver.send_message("On")
+    Octavius_Receiver.send_message("Off")
+    Octavius_Receiver.send_message("Hold")
+    Octavius_Receiver.send_message("Talk")
+    Octavius_Receiver.send_message("Reboot")
+    Octavius_Receiver.send_message("Update")
+    Octavius_Receiver.send_message("Print files")
+    Octavius_Receiver.send_message("Print (filename)")
+    Octavius_Receiver.send_message("Length (filename)")
+    Octavius_Receiver.send_message("Delete (filename)")
+    return
 
 def on():
     led.on()
-    sleep(1)
-    led.off()
-    print("Turning computer on")
     return
 
 def off():
     led.off()
-    print("Turning computer off")
     return
 
-def hold(duration, Octavius_Receiver):
-    Octavius_Receiver.send_message("Holding...")
-    led.on()
-    print("Pressed")
+def hold(duration):
+    on()
     sleep(duration)
-    led.off()
-    Octavius_Receiver.send_message("...Released")
-
-    print("Released")
+    off()
     return
 
-'''
-def Self_Update():
-    os.chdir("..")
-    updater_filepath = os.path.abspath(os.curdir) + "\\Updater.py"
-    print("Running updater")
-    update_file_dir = "Update_Files"
-    
-    with open(updater_filepath) as f:
-        exec(f.read())
-    
-    if os.path.exists(update_file_dir):
-        shutil.rmtree(update_file_dir)
-
-    #    os.remove(update_file_dir)
-
-    clone_repository("https://github.com/8BitFishy/wollybot", update_file_dir,
-                     bare=False, repository=None, remote=None, checkout_branch=None, callbacks=None)
-
-    import sys, subprocess
-    subprocess.run(["python", updater_filepath])
-    subprocess.Popen("python -c \"import os, time; time.sleep(1); os.remove('{}');\"".format(sys.argv[0]))
-    sys.exit(0)
-
-
-    #exit()
-    #os.chdir("..")
-    #updater_filepath = os.path.abspath(os.curdir) + "\\Updater.py"
-    #subprocess.Popen(f'python3 {updater_filepath}')
-
-    #r = requests.get('https://raw.githubusercontent.com/8BitFishy/wollybot/wolly_bot2.py')
-    #print(r)
-
-
-
-
-def Restart():
+def reboot():
+    system("sudo reboot")
     return
 
-'''
+def delete(filename):
+    remove(filename)
+    return
+
+
 
 def handle(msg, Octavius_Receiver):
-    duration = 0
-    print(msg)
-    #rawcommand = msg['text']
-    #capcommand = msg.upper()
 
     command = msg.split()
-    action = command[0]
+    action = command[0].upper()
 
-    print('Got command: %s' % command)
-    print("{}".format(action), end="")
+    if action == "HELLO":
+        Octavius_Receiver.send_message("Hello, what can I do for you?")
 
-    if action == 'ON':
-        print()
+    elif action == 'ON' or action == "OFF":
+
         try:
-            on()
-            Octavius_Receiver.send_message("Turning computer on")
-        except NameError:
-            Octavius_Receiver.send_message("LED package not recognised, are you sure this is a pi?")
+            if action == "ON":
+                modifier = "on"
+            else:
+                modifier = "off"
 
-    elif action == 'OFF':
-        print()
-        try:
-            off()
-            Octavius_Receiver.send_message("Turning computer off")
-        except NameError:
-            Octavius_Receiver.send_message("LED package not recognised, are you sure this is a pi?")
+            Octavius_Receiver.send_message("Turning computer " + modifier)
+            print(ctime() + " - Action - Turning computer " + modifier)
+            hold(1)
+
+        except Exception as E:
+            handle_error(E, Octavius_Receiver)
 
     elif action == 'TALK':
-        print()
-        Octavius_Receiver.send_message("Hello, what can I do for you?")
+        talk(Octavius_Receiver)
 
     elif action == "HOLD":
         try:
             duration = int(command[1])
-            print(" {}".format(duration))
-            hold(duration, Octavius_Receiver)
-        except NameError:
-            Octavius_Receiver.send_message("LED package not recognised, are you sure this is a pi?")
-    else:
-        print()
-        print("Command not recognised")
-        Octavius_Receiver.send_message("Command not recognised")
-    
-    '''
-    elif action == "UPDATE":
-        print()
-        try:
-            Octavius_Receiver.send_message("Are you sure you want to update? Yes / No")
-            response = Octavius_Receiver.get_response()
-            if response == "YES":
-                print("Running self update")
-                Self_Update()
-            else:
-                pass
-        except Exception as e:
-            print(e)
-    '''
+            Octavius_Receiver.send_message(f"Holding button for {duration} seconds")
+            print(ctime() + " - Action - Hold " + str(duration))
+            hold(duration)
 
+        except Exception as E:
+            handle_error(E, Octavius_Receiver)
+
+
+    elif action == "PRINT" and command[1] != "files":
+
+        filename = str(command[1])
+        print(ctime() + " - Action - Sending file: ")
+        print(f'{directory}{filename}')
+        Octavius_Receiver.send_message(f"Accessing {filename}")
+
+        try:
+            f = open(f'{directory}{filename}')
+            if len(command) == 3:
+                for line in (f.readlines()[-int(command[2]):]):
+                    Octavius_Receiver.send_message(str(line).strip())
+
+            else:
+                Octavius_Receiver.send_message(str(f.read()))
+            f.close()
+
+        except Exception as E:
+            handle_error(E, Octavius_Receiver)
+
+
+    elif action == "PRINT" and command[1] == "files":
+
+        print(ctime() + " - Action - Read file names")
+
+        try:
+            current_directory = __file__.strip("Command_Centre.py").strip(":")
+            print(ctime() + " - Searching directory: \n" + current_directory)
+            Octavius_Receiver.send_message("Files found:")
+
+            for f in listdir(current_directory):
+                if isfile(join(current_directory, f)):
+                    Octavius_Receiver.send_message(f)
+
+        except Exception as E:
+            handle_error(E, Octavius_Receiver)
+
+
+    elif action == "REBOOT":
+        print(ctime() + " - Rebooting")
+        Octavius_Receiver.send_message("Rebooting")
+
+        try:
+            reboot()
+
+        except Exception as E:
+            handle_error(E, Octavius_Receiver)
+
+
+    elif action == "DELETE":
+        filename = str(command[1])
+        if filename not in protected_files:
+            print(ctime() + " - Action - Deleting file: " + filename)
+            Octavius_Receiver.send_message(f"Deleting {filename}")
+
+            try:
+                delete(f'{directory}{filename}')
+                Octavius_Receiver.send_message(f"{filename} deleted")
+
+            except Exception as E:
+                handle_error(E, Octavius_Receiver)
+
+        else:
+            Octavius_Receiver.send_message("That file is protected, please don't try to delete this")
+
+
+    elif action == "LENGTH":
+
+        filename = str(command[1])
+        print(ctime() + " - Action - Sending length of file: " + filename)
+        Octavius_Receiver.send_message(f"Reading length of {filename}")
+
+        try:
+            with open(f'{directory}{filename}') as file:
+                count = sum(1 for _ in file)
+                Octavius_Receiver.send_message(f"File is {count} lines long")
+                file.close()
+
+        except Exception as E:
+            handle_error(E, Octavius_Receiver)
+
+
+    elif action == "UPDATE":
+        try:
+            print(ctime() + " - Action - Update")
+            Octavius_Receiver.send_message(f"Updating files")
+            update()
+            print(ctime() + " - Update Complete")
+            Octavius_Receiver.send_message(f"Update complete, rebooting")
+            reboot()
+
+        except Exception as E:
+            handle_error(E, Octavius_Receiver)
+
+
+    else:
+        print(ctime() + " - No action - Command not recognised")
+        Octavius_Receiver.send_message("Command not recognised")
--- Telegram_Manager.py
+++ Telegram_Manager.py
@@ -1,11 +1,12 @@
 import json
 import requests
 import urllib
-from time import time
+from time import time, ctime, sleep
 
 filename = 'telegramID.txt'
+directory = __file__.strip("Telegram_Manager.py").strip(":")
 
-with open(filename) as f:
+with open(f'{directory}{filename}') as f:
     IDS = f.read().splitlines()
 
 chat_id = str(IDS[0])
@@ -13,9 +14,9 @@ TOKEN = str(IDS[1])
 URL = "https://api.telegram.org/bot{}/".format(TOKEN)
 
 class Message_Receiver:
-    def __init__(self, text, last_update_id):
+    def __init__(self, text):
         self.text = text
-        self.last_update_id = last_update_id
+        self.last_update_id = None
 
     
     def get_url(self, url):
@@ -40,66 +41,63 @@ class Message_Receiver:
         for update in updates["result"]:
             update_ids.append(int(update["update_id"]))
         return max(update_ids)
-    
-    def get_last_chat_id_and_text(self, updates):
-        num_updates = len(updates["result"])
-        last_update = num_updates - 1
-        text = updates["result"][last_update]["message"]["text"]
-        chat_id = updates["result"][last_update]["message"]["chat"]["id"]
-        return (text, chat_id)
-    
+
     def send_message(self, text):
-        text = text.capitalize()
-        text = urllib.parse.quote_plus(text)
-        url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
-        self.get_url(url)
+
+        try:
+            print(ctime() + " - Sending Message - " + text)
+            text = urllib.parse.quote_plus(text)
+            url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
+            self.get_url(url)
+
+        except Exception as e:
+            print(f"{ctime()} - Error reaching URL, cannot send message")
+            print(e)
+
+
     
     def get_response(self):
+        self.text = ""
+        try:
+            updates = self.get_updates(self.last_update_id)
 
-        print("Entering get response")
-        updates = self.get_updates(self.last_update_id)
-        print(f"Update ID = {self.last_update_id}")
-        print(updates)
+            if len(updates["result"]) is not None:
 
-        if len(updates["result"]) > 0:
-            date_time = int(str(time()).split(".")[0])
-            time_since_message = updates["result"][0]["message"]["date"] - date_time
+                if len(updates["result"]) > 0:
 
-            if abs(time_since_message) < 20 and self.last_update_id is not None:
-                print("Update found")
-                print("Update ID = {}".format(self.last_update_id))
-                self.text = updates["result"][0]["message"]["text"]
-                print("Received update = {}".format(updates["result"][0]["message"]["text"]))
-                self.last_update_id = self.get_last_update_id(updates) + 1
+                    self.last_update_id = int(updates["result"][0]["update_id"])
 
+                    print(ctime() + " - Received Update: ")
+                    print(updates)
 
-            else:
-                print("Message timeout")
-                self.text=''
-                self.last_update_id = self.get_last_update_id(updates) + 1
+                    date_time = int(str(time()).split(".")[0])
+                    time_since_message = updates["result"][0]["message"]["date"] - date_time
+                    self.last_update_id = self.get_last_update_id(updates) + 1
 
+                    if abs(time_since_message) < 20:
+                        self.text = updates["result"][0]["message"]["text"]
+                        print(ctime() + ' - Update Text - "' + self.text + '"')
 
-        else:
+                    else:
+                        print(ctime() + " - Message timed out")
+
+            return self.text
+
+        except Exception as e:
+            print("Caught exception")
+            try:
+                if str(updates["error_code"]) == str(409):
+                    print("Is 409 error")
+                    exit()
+
+            except:
+                pass
+            print(f"{ctime()} - Error reaching URL, cannot get updates")
+            print(e)
             self.text = ''
-
-        self.text = self.text.upper()
-        print("Leaving get response with text: {}".format(self.text))
-        return self.text
-    
-    
-    def get_confirmation(self, Octavius_Vocab):
-    
-        response = self.get_response()
-
-        if response in Octavius_Vocab.affirmativelist:
-            return True
-        else:
-            return False
-
-
-
-
+            sleep(5)
+            return self.text
 
 def generate_receiver():
-    Octavius_Receiver = Message_Receiver("", None)
+    Octavius_Receiver = Message_Receiver("")
     return Octavius_Receiver
